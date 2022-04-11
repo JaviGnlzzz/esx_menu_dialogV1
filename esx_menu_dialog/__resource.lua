2e1714e19e914870c2bf0268e3d726686019fdc7
Javi.Gnlz <98654716+JaviGnlzzz@users.noreply.github.com>
2022-04-11 13:45:57 +0200

    Add files via upload

---
+++ esx_menu_dialog/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'TusMuertos.#4903'
+
+description 'esx_menu_dialog V1'
+
+client_script 'client/main.lua'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+}
+
+dependency 'es_extended'
